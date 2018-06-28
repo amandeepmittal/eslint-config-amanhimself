9b8042a8a4f34772ea4f18f730116111cceb7938
Aman Mittal <amandeepmittal@live.com>
2018-06-29 02:00:47 +0530

    add all rules

---
+++ index.js
@@ -0,0 +1,63 @@
+module.exports = {
+	extends: [
+		'./rules/best-practices',
+		'./rules/errors',
+		'./rules/es6',
+		'./rules/imports',
+		'./rules/react-a11y',
+		'./rules/react'
+	].map(require.resolve),
+	parserOptions: {
+		ecmaVersion: 7,
+		sourceType: 'module'
+	},
+	plugins: ['class-property', 'import'],
+	parser: 'babel-eslint',
+	rules: {
+		strict: 0,
+		'react/jsx-indent': 0,
+		'react/jsx-filename-extension': 0,
+		'react/prop-types': 0,
+		'react/no-did-mount-set-state': 0,
+		'react/prefer-stateless-function': 0,
+		'no-trailing-spaces': 0,
+		'import/prefer-default-export': 0,
+		'import/no-extraneous-dependencies': 0,
+		'new-cap': 0,
+		'func-names': 0,
+		'consistent-return': 0,
+		'no-use-before-define': 0,
+		'global-require': 0,
+		'linebreak-style': 0
+	},
+	globals: {
+		describe: false,
+		it: false,
+		test: false,
+		expect: false,
+		beforeEach: false,
+		afterEach: false,
+		before: false,
+		__DEV__: false,
+		Atomics: false,
+		ErrorUtils: false,
+		FormData: false,
+		SharedArrayBuffer: false,
+		XMLHttpRequest: false,
+		alert: false,
+		cancelAnimationFrame: false,
+		cancelIdleCallback: false,
+		clearImmediate: false,
+		clearInterval: false,
+		clearTimeout: false,
+		fetch: false,
+		navigator: false,
+		process: false,
+		requestAnimationFrame: false,
+		requestIdleCallback: false,
+		setImmediate: false,
+		setInterval: false,
+		setTimeout: false,
+		window: false
+	}
+};
